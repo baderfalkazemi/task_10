3a2a0b19eb36d4889e2d87abb59a7e3b1a3ee422
baderfalkazemi <bader.alkazemi@gmail.com>
2018-12-08 14:51:55 +0300

    task10

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=120)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     description = models.TextField()
     opening_time = models.TimeField()
     closing_time = models.TimeField()
@@ -9,3 +11,12 @@ class Restaurant(models.Model):
 
     def __str__(self):
     	return self.name
+
+class Item(models.Model):
+	name = models.CharField(max_length=100)
+	description = models.CharField(max_length=100)
+	price = models.DecimalField(max_digits=6, decimal_places=3)
+	restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.name
